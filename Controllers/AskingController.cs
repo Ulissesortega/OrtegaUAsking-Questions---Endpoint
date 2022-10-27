7f5022bd3be01ad95444624768cba7b0f5e9fad6
Ulissesortega <Ulisses.ortega@hotmail.com>
2022-10-27 14:17:12 -0700

    GitHub Upload

---
+++ Controllers/AskingController.cs
@@ -0,0 +1,30 @@
+//Name: Ulises Ortega
+//Date: 10.25.22
+//Project: Asking Questions
+//Description: This Endpoint request two inputs from the user and then it will outp the name and the time the user woke up.
+//Peer Review: Chris Martinez, The program is working as intended, I was able to add name in the route line, along with time and it was able to generate
+//an answer.
+
+
+//Description: in the example bellow your local host will change,
+//When you run and degub this API in your computer the local host will change
+//folow the structure below and replace Answer1 with your Name and Answer 2 with the time that you woke up and press enter
+//https://localhost:7108/asking/questions/Answer1/Answer2  
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrtegaUAsking_Questions___Endpoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AskingController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("Questions/{name}/{time}")]
+    public string Questions(string name, string time)
+    {
+        return $"What is your name: {name} What time did you woke up today: {time}";
+    }
+}
